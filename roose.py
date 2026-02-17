80a60cc9d896f3780c0efc1af5698a77f5e3526f
artsancc <arzunurtunal@gmail.com>
2026-02-17 14:22:54 +0300

    Add files via upload

---
+++ roose.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('rose.jpeg')
+# Görseli katmanlara bölme işlemi
+b, g, r = cv2.split(img)
+fark = cv2.subtract(r, g)
+# Otsu ile eşikleme işlemi
+ret, maske = cv2.threshold(fark, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+# Otsu'nun hesapladığı sayıyı görmek için
+print(f"Otsu'nun hesapladığı eşik değeri: {ret}")
+# Renk değiştirme işlemi
+hsv_resim = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+hsv_resim[maske == 255, 0] = 145
+mor_resim = cv2.cvtColor(hsv_resim, cv2.COLOR_HSV2BGR)
+# Arka planı karartma
+sonuc = cv2.bitwise_and(mor_resim, mor_resim, mask=maske)
+
+cv2.imshow('OTSU MASK', maske)
+cv2.imshow('SONUC', sonuc)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
