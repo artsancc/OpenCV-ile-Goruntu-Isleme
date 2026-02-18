3abf6cbf31fb356a9d3583b4079bb4588517e58a
artsancc <arzunurtunal@gmail.com>
2026-02-18 09:21:48 +0300

    Update roose.py

--- roose.py
+++ roose.py
@@ -1,28 +1,22 @@
 import cv2
-import numpy as np
+from src.kernelHelper import maske_olustur, rengi_degistir, arka_plani_karart
 
-img = cv2.imread('rose.jpeg')
-# Görseli katmanlara bölme işlemi
-b, g, r = cv2.split(img)
-fark = cv2.subtract(r, g)
-# Otsu ile eşikleme işlemi
-ret, maske = cv2.threshold(fark, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-# Otsu'nun hesapladığı sayıyı görmek için
-print(f"Otsu'nun hesapladığı eşik değeri: {ret}")
-# Kernel ile temizleme işlemi
-kernel = np.ones((3,3), np.uint8)
-maske = cv2.morphologyEx(maske, cv2.MORPH_CLOSE, kernel, iterations=2)
-maske = cv2.morphologyEx(maske, cv2.MORPH_OPEN, kernel, iterations=1)
-# Renk değiştirme işlemi
-hsv_resim = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-hsv_resim[maske == 255, 0] = 145
-mor_resim = cv2.cvtColor(hsv_resim, cv2.COLOR_HSV2BGR)
-# Arka planı karartma
-sonuc = cv2.bitwise_and(mor_resim, mor_resim, mask=maske)
+# Görseli yükle
+img = cv2.imread("rose.jpeg")
 
-cv2.imshow('OTSU MASK', maske)
-cv2.imshow('SONUC', sonuc)
+# Maske oluştur
+maske, otsu_degeri = maske_olustur(img)
+print(f"Otsu'nun hesapladığı eşik değeri: {otsu_degeri}")
+
+# Rengi değiştir
+mor_resim = rengi_degistir(img, maske)
+
+# Arka planı karart
+sonuc = arka_plani_karart(mor_resim, maske)
+
+# Görselleri göster
+cv2.imshow("OTSU MASK", maske)
+cv2.imshow("SONUC", sonuc)
 
 cv2.waitKey(0)
-
 cv2.destroyAllWindows()
