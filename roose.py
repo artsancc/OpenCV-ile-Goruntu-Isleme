6accaf100f2f8fa250eaec47adb9329493fca5d3
artsancc <arzunurtunal@gmail.com>
2026-02-18 09:16:34 +0300

    Update roose.py
    Kernel ile temizleme işlemi yapılarak daha temiz bir görüntü sağlanmıştır.

--- roose.py
+++ roose.py
@@ -9,6 +9,10 @@ fark = cv2.subtract(r, g)
 ret, maske = cv2.threshold(fark, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 # Otsu'nun hesapladığı sayıyı görmek için
 print(f"Otsu'nun hesapladığı eşik değeri: {ret}")
+# Kernel ile temizleme işlemi
+kernel = np.ones((3,3), np.uint8)
+maske = cv2.morphologyEx(maske, cv2.MORPH_CLOSE, kernel, iterations=2)
+maske = cv2.morphologyEx(maske, cv2.MORPH_OPEN, kernel, iterations=1)
 # Renk değiştirme işlemi
 hsv_resim = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 hsv_resim[maske == 255, 0] = 145
@@ -20,4 +24,5 @@ cv2.imshow('OTSU MASK', maske)
 cv2.imshow('SONUC', sonuc)
 
 cv2.waitKey(0)
+
 cv2.destroyAllWindows()
